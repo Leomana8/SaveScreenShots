2cae6b1723c7525360f9180b91aecfa7370ecb46
Leomana8 <leomana@rambler.ru>
2014-03-03 21:19:58 +0400

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace SaveScreenshots
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.bOk = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbNameFile = new System.Windows.Forms.TextBox();
+            this.cbDate = new System.Windows.Forms.CheckBox();
+            this.tbFolder = new System.Windows.Forms.TextBox();
+            this.bBrowse = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bOk
+            // 
+            this.bOk.Enabled = false;
+            this.bOk.Location = new System.Drawing.Point(188, 218);
+            this.bOk.Name = "bOk";
+            this.bOk.Size = new System.Drawing.Size(126, 32);
+            this.bOk.TabIndex = 2;
+            this.bOk.Text = "Приступить и скрыть";
+            this.bOk.UseVisualStyleBackColor = true;
+            this.bOk.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "SaveScreenshots";
+            this.notifyIcon1.Visible = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(9, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(270, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Введите имя файлов скриншотов";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // tbNameFile
+            // 
+            this.tbNameFile.Location = new System.Drawing.Point(13, 40);
+            this.tbNameFile.MaxLength = 60;
+            this.tbNameFile.Name = "tbNameFile";
+            this.tbNameFile.Size = new System.Drawing.Size(365, 20);
+            this.tbNameFile.TabIndex = 4;
+            this.tbNameFile.TextChanged += new System.EventHandler(this.tbNameFile_TextChanged);
+            // 
+            // cbDate
+            // 
+            this.cbDate.AutoSize = true;
+            this.cbDate.Location = new System.Drawing.Point(13, 66);
+            this.cbDate.Name = "cbDate";
+            this.cbDate.Size = new System.Drawing.Size(210, 17);
+            this.cbDate.TabIndex = 5;
+            this.cbDate.Text = "Использовать дату и время в имени";
+            this.cbDate.UseVisualStyleBackColor = true;
+            // 
+            // tbFolder
+            // 
+            this.tbFolder.Location = new System.Drawing.Point(13, 43);
+            this.tbFolder.Name = "tbFolder";
+            this.tbFolder.Size = new System.Drawing.Size(365, 20);
+            this.tbFolder.TabIndex = 1;
+            this.tbFolder.Click += new System.EventHandler(this.tbFolder_Click);
+            this.tbFolder.TextChanged += new System.EventHandler(this.tbFolder_TextChanged);
+            // 
+            // bBrowse
+            // 
+            this.bBrowse.Location = new System.Drawing.Point(384, 40);
+            this.bBrowse.Name = "bBrowse";
+            this.bBrowse.Size = new System.Drawing.Size(75, 23);
+            this.bBrowse.TabIndex = 6;
+            this.bBrowse.Text = "Обзор...";
+            this.bBrowse.UseVisualStyleBackColor = true;
+            this.bBrowse.Click += new System.EventHandler(this.bBrowse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Выберете путь для сохранения скриншотов";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.bBrowse);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.tbFolder);
+            this.groupBox1.Location = new System.Drawing.Point(20, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(471, 75);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbNameFile);
+            this.groupBox2.Controls.Add(this.cbDate);
+            this.groupBox2.Location = new System.Drawing.Point(20, 108);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(471, 91);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 267);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.bOk);
+            this.Name = "Form1";
+            this.Text = "SaveScreenShots";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button bOk;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbNameFile;
+        private System.Windows.Forms.CheckBox cbDate;
+        private System.Windows.Forms.TextBox tbFolder;
+        private System.Windows.Forms.Button bBrowse;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using Utilities; // hook
+
+namespace SaveScreenshots
+{
+    public partial class Form1 : Form
+    {
+        String pathToFolder;
+        Int32 ImageIndex;
+        String namefile;
+        globalKeyboardHook gkh = new globalKeyboardHook();
+        public Form1()
+        {
+            InitializeComponent();
+            pathToFolder = "";
+            ImageIndex = 0;
+            KeyPreview = true;
+            tbFolder.TabStop = false;
+            notifyIcon1.Visible = false;
+            
+            // событие двойного щелчка на трей
+            notifyIcon1.MouseDoubleClick += new MouseEventHandler(notifyIcon1_MouseDoubleClick);
+            
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            namefile = "sss"; // имя по умолчанию
+            // добавление заданного имени файла
+            // проверка задали ли имя файлу
+            if(!String.IsNullOrEmpty(tbNameFile.Text))
+            {
+                namefile = tbNameFile.Text;
+            }
+
+            // делаем нашу иконку в трее активной
+            notifyIcon1.Visible = true;
+            gkh.HookedKeys.Add(Keys.PrintScreen);
+            gkh.KeyUp += new KeyEventHandler(gkh_KeyUp);
+            
+            this.Hide();
+         
+        }
+        void gkh_KeyUp(object sender, KeyEventArgs e)
+        {
+            SaveScreen();
+            
+        }
+        private void SaveScreen()
+        {
+            var image = Clipboard.GetImage();
+            if (image != null)
+            {
+                if(cbDate.Checked)
+                {
+                    image.Save(pathToFolder + @"\" + namefile + "_" + 
+                        DateTime.Now.ToString("dd.MM.yy_HH.mm.ss.f") + ".png", ImageFormat.Png);
+                }
+                else
+                {
+                    image.Save(pathToFolder + @"\" + namefile + "_" + ++ImageIndex + ".png", ImageFormat.Png);
+                }
+
+            }
+        }
+
+        private void tbFolder_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            // делаем нашу иконку скрытой
+            notifyIcon1.Visible = false;
+            this.Show();
+        }
+
+        private void tbFolder_TextChanged(object sender, EventArgs e)
+        {
+            // если поле не введено -  кнопку не показывать
+            if (tbFolder.Text != "")
+                bOk.Enabled = true;
+            else
+                bOk.Enabled = false;
+
+            if (!System.IO.Directory.Exists(tbFolder.Text))
+            {
+                tbFolder.ForeColor = Color.Red;
+                bOk.Enabled = false;
+            }
+            else
+            {
+                tbFolder.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tbNameFile_TextChanged(object sender, EventArgs e)
+        {
+            if (tbNameFile.Text.IndexOfAny(System.IO.Path.GetInvalidFileNameChars()) != -1)
+            {
+                tbNameFile.ForeColor = Color.Red;
+                bOk.Enabled = false;
+            }
+            else
+            {
+                tbNameFile.ForeColor = Color.Black;
+            }
+
+        }
+
+
+        private void bBrowse_Click(object sender, EventArgs e)
+        {
+            // показать диалог выбора пути папки
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                tbFolder.Text = folderBrowserDialog1.SelectedPath;
+                pathToFolder = folderBrowserDialog1.SelectedPath;
+            }
+            // если поле пути не введено - кнопку не показывать
+            if (tbFolder.Text != "")
+                bOk.Enabled = true;
+            else
+                bOk.Enabled = false;
+        }
+
+
+
+    }
+}
